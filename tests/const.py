bd5906cf60d30489fb4103322b26de8e8d8c41ed
Andrzej Chmielewski <andrzejchm@gmail.com>
2022-06-09 13:11:32 +0200

    initial commit

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for BleBox shutterBox with tilt tests."""
+from custom_components.blebox_shutterbox_tilt.const import (
+    CONF_PASSWORD,
+)
+from custom_components.blebox_shutterbox_tilt.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
